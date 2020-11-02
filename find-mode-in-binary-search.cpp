93255675d7d345d6e9a2bd3eaab24b077ce1c979
Rane Wallin <github@blasph.me>
2020-11-02 09:58:31 -0700

    Find modes in BST

---
+++ find-mode-in-binary-search.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    std::map<int, int> modes;
+    vector<int> findMode(TreeNode *root)
+    {
+        std::vector<int> highestModes;
+        int highestCount = 0;
+
+        // Create a map from the tree where the
+        // keys are the node values and the values are
+        // the number of times that node value appeared
+        getModes(root);
+
+        // iterate through the map keys
+        for (std::map<int, int>::iterator it = modes.begin(); it != modes.end(); it++)
+        {
+            int key = it->first;
+            int val = it->second;
+
+            // If the number of times a key appears is more than the number of times of
+            // the current memoized key(s) (highestModes) appeared, replace the highestModes
+            // and the highestCount with the new values
+            if (val > highestCount)
+            {
+                highestCount = val;
+                highestModes.clear();
+                highestModes.push_back(key);
+                // If the number of time key appears is equal, add the key to the
+                // highestModes vector
+            }
+            else if (val == highestCount)
+            {
+                highestModes.push_back(key);
+            }
+        }
+
+        return highestModes;
+    }
+
+    void getModes(TreeNode *root)
+    {
+        int tmpVal = root->val;
+
+        if (tmpVal)
+        {
+            modes[tmpVal] = modes[tmpVal] + 1;
+
+            if (root->right)
+                getModes(root->right);
+
+            if (root->left)
+                getModes(root->left);
+        }
+    }
+};
